3f63b0533d903fcf0284a827ce4ffa8bd8be4222
wasilewski94 <wasilewskikrzysztof94@gmail.com>
2018-04-07 15:25:14 +0200

    dodane parsowanie katow AZ i EL, moze byc problem gdy w nazwie satelity bedzie 'AZ', 'EL' lub '.'

--- komunikacja_z_rotorem/serial/serial.ino
+++ komunikacja_z_rotorem/serial/serial.ino
@@ -16,8 +16,12 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(10, 11); // RX, TX
-int bufor = 0;
-  
+String Data = "";
+String Azymut = "";
+String Elewacja = "";
+float AZvalue = 0.0;
+float ELvalue = 0.0;
+
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
@@ -25,23 +29,49 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   Serial.println("Serial connection success!");
-
-  // set the data rate for the SoftwareSerial port
+  Serial.println("");
+  mySerial.begin(9600);  // set the data rate for the SoftwareSerial port
 }
 
 void loop() {
-  
-  mySerial.begin(9600);
-  //bufor = mySerial.read();
-  //Serial.println(bufor);
-  //Serial.println("odebrano!");
-  //delay(10);
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
-  }
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
-  }
-  
+
+    while (mySerial.available())
+    {
+        char character = mySerial.read(); // Receive a single character from the software serial port
+        Data.concat(character); // Add the received character to the receive buffer
+        if (character == '\n')
+        {
+            Serial.print("Received: " + Data);
+
+            // Add your code to parse the received line here....
+            int AZtag = Data.indexOf("AZ");
+            int ELtag = Data.indexOf("EL");
+            int firstDot = Data.indexOf(".");
+            int secondDot = Data.indexOf('.', firstDot + 1);
+
+            //Serial.println("The index of the AZ tag in the string " + Data + " is " + AZtag);
+            //Serial.println("The index of the EL tag in the string " + Data + " is " + ELtag);
+            //Serial.println("The index of . in the string " + Data + " is " + firstDot);
+            //Serial.println("The index of second . in the string " + Data + " is " + secondDot);
+
+            Azymut = Data.substring(AZtag+2, firstDot+2);
+            Elewacja = Data.substring(ELtag+2, secondDot+2);
+            AZvalue = Azymut.toFloat();
+            ELvalue = Elewacja.toFloat();
+            
+            Serial.println("");
+            Serial.println("Azymut: ");
+            Serial.println(AZvalue);
+            Serial.println("Elewacja: ");
+            Serial.println(ELvalue);
+            Serial.println("");
+      
+            // Clear receive buffer so we're ready to receive the next line
+            Data = "";
+            Azymut = "";
+            Elewacja = "";
+        }
+    }
+
   
 }
