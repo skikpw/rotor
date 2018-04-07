cfdb39e83dfc897d9798567edcc74fb890d33fed
wasilewski94 <wasilewskikrzysztof94@gmail.com>
2018-04-07 13:41:17 +0200

    dziala wysylanie z orbitrona komend, brak parsowania katoa AZ i EL
    komunikacja między arduino a Orbitronem

---
+++ komunikacja_z_rotorem/serial/serial.ino
@@ -0,0 +1,47 @@
+/*
+  Software serial multple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+
+ Not all pins on the Leonardo and Micro support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+int bufor = 0;
+  
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Serial connection success!");
+
+  // set the data rate for the SoftwareSerial port
+}
+
+void loop() {
+  
+  mySerial.begin(9600);
+  //bufor = mySerial.read();
+  //Serial.println(bufor);
+  //Serial.println("odebrano!");
+  //delay(10);
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+  
+  
+}
